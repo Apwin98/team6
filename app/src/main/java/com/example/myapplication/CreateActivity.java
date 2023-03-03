de86f6c7f7a0623a6d66c3412e5dd9361dd4e58a
anthony.p.nguyen <apnguyen98@gmail.com>
2023-03-02 21:34:42 -0600

    connected firebase sdk and implemented add attraction button

---
+++ app/src/main/java/com/example/myapplication/CreateActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class CreateActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+
+
+    }
+}
