5f94b72edc540936e443610dff67fb5d037d8f6f
anthony.p.nguyen <apnguyen98@gmail.com>
2023-03-02 21:21:57 -0600

    connected firebase sdk and implemented add attraction button

--- app/src/main/java/com/example/myapplication/SecondFragment.java
+++ app/src/main/java/com/example/myapplication/SecondFragment.java
@@ -1,24 +1,33 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.myapplication.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
 
+
+    private static final String KEY_CITY = "city";
+    private static final String KEY_LOCATION_NAME = "location_name";
+    private static final String KEY_DESCRIPTION = "description";
+
+    private EditText editTextCity;
+    private EditText editLocationName;
     private FragmentSecondBinding binding;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
+
     ) {
 
         binding = FragmentSecondBinding.inflate(inflater, container, false);
@@ -28,7 +37,13 @@ public class SecondFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), CreateActivity.class);
+                view.getContext().startActivity(intent);
+            }
+        });
 
     }
 
