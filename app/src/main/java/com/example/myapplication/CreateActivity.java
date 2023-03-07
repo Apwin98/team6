0d02d411afc2c8edc16672b0ff82857a76f81e9e
vorridge <63434421+vorridge@users.noreply.github.com>
2023-03-06 22:14:27 -0600

    Upload Image to Storage and Keys (Viena)

--- app/src/main/java/com/example/myapplication/CreateActivity.java
+++ app/src/main/java/com/example/myapplication/CreateActivity.java
@@ -1,41 +1,62 @@
 package com.example.myapplication;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.EventLogTags;
+import android.provider.OpenableColumns;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.myapplication.databinding.ActivityCreateBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 
 public class CreateActivity extends AppCompatActivity {
     ActivityCreateBinding binding;
     FirebaseDatabase db;
+
+    StorageReference storageReference;
     DatabaseReference reference;
-     Spinner City, Category;
+
+
+    Spinner City, Category;
     String categoryItem, cityItem, Locationname, Description, Address;
 
+    //Upload Images
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding =  ActivityCreateBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
 
 
         ActionBar actionBar = getSupportActionBar();
@@ -65,8 +86,6 @@ public class CreateActivity extends AppCompatActivity {
             }
         });
 
-
-
         //City Dropdown
         Spinner City = findViewById(R.id.cityDropDwn);
         ArrayList<String> cities = new ArrayList<>();
@@ -88,30 +107,133 @@ public class CreateActivity extends AppCompatActivity {
 
             }
         });
+
+
+        //Image Activity Launcher
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        TextView imageName = (TextView) findViewById(R.id.imageName);
+
+                        if (result.getResultCode() == Activity.RESULT_OK){
+                            Intent data = result.getData();
+                            imageUri = data.getData();
+                            imageName.setText(getFileName(imageUri));
+
+                        }else {
+                            Toast.makeText(CreateActivity.this, "No Image Selected", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                }
+        );
+        //Upload Images
+        Button uploadBtn = findViewById(R.id.uploadBtn);
+        uploadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPicker = new Intent();
+                photoPicker.setAction(Intent.ACTION_GET_CONTENT);
+                photoPicker.setType("image/*");
+                activityResultLauncher.launch(photoPicker);
+
+            }
+
+        });
+
+
         binding.submitAtt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Locationname = binding.nameInput.getText().toString();
                 Description = binding.decriptionInput.getText().toString();
                 Address = binding.streetInput.getText().toString();
-                if(!Locationname.isEmpty() && !Description.isEmpty() && !Address.isEmpty() && !cityItem.isEmpty() && !categoryItem.isEmpty()){
 
-                    Locations locations = new Locations(Locationname, Description, Address, cityItem, categoryItem);
+                if(!Locationname.isEmpty() && !Description.isEmpty() && !Address.isEmpty() && !cityItem.isEmpty() && !categoryItem.isEmpty() && (imageUri != null)){
+
                     db = FirebaseDatabase.getInstance();
                     reference = db.getReference("Locations");
+                    String key = reference.push().getKey();
+
+                    Locations locations = new Locations(Locationname, Description, Address, cityItem, categoryItem, key);
+
+                    storageReference = FirebaseStorage.getInstance().getReference("Images").child(getFileExt(imageUri));
+                    //.getInstance().getReference("Locations");
+
                     reference.child(Locationname).setValue(locations).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             binding.nameInput.setText("");
                             binding.decriptionInput.setText("");
                             binding.streetInput.setText("");
+
+                            storageReference.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                    storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                        @Override
+                                        public void onSuccess(Uri uri) {
+
+                                            //reference.child(Locationname).child(key).setValue(imageUri.toString());
+                                        }
+                                    });
+                                }
+                            });
+
+                            Intent intent = new Intent(CreateActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+
                             //Toast.makeText(CreateActivity.this, "Successful something!", Toast.LENGTH_SHORT).show();
                         }
+
                     });
+
+                }else{
+                    Toast.makeText(CreateActivity.this, "Fill out all parts", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
     }
+    private String getFileExt(Uri fileUri){
+
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+
+        return mime.getExtensionFromMimeType(contentResolver.getType(fileUri));
+    }
+
+    //Get image file name
+    @SuppressLint("Range")
+    private String getFileName(Uri imageUri) {
+//        String path = imageUri.toString();
+//        int lastSlashIndex = path.lastIndexOf("/");
+//        return path.substring(lastSlashIndex + 1, path.length());
+        String result = null;
+        if (imageUri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(imageUri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = imageUri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
+
     //back button
     public boolean onOptionsItemSelected( MenuItem item) {
         switch (item.getItemId()) {
