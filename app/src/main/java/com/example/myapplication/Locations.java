6087e7f45fd120df6180a5f9e908745e78d13d59
vorridge <63434421+vorridge@users.noreply.github.com>
2023-03-08 22:06:28 -0600

    Upload Image to Storage and Keys

--- app/src/main/java/com/example/myapplication/Locations.java
+++ app/src/main/java/com/example/myapplication/Locations.java
@@ -1,17 +1,24 @@
 package com.example.myapplication;
 
+import android.net.Uri;
+import android.net.UrlQuerySanitizer;
+
+import java.net.URL;
+
 public class Locations {
-    public String Location, Description, Address, City, Category;
+    public String Location, Description, Address, City, Category, Key;
+
 
     public Locations(){}
 
-    public Locations(String Location,String Description, String Address, String City, String Category ){
+    public Locations(String Location, String Description, String Address, String City, String Category,String Key){
 
         this.Location = Location;
         this.Description = Description;
         this.Address = Address;
         this.City = City;
         this.Category = Category;
+        this.Key = Key;
 
     }
 
