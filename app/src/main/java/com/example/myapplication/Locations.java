df245cf99f85a4787ce5fc88848cab733f5867ff
BryanCorona0524 <bryan.coronarangel@mavs.uta.edu>
2023-03-11 22:41:12 -0600

    Query now gets appropriate locations based on city/category

--- app/src/main/java/com/example/myapplication/Locations.java
+++ app/src/main/java/com/example/myapplication/Locations.java
@@ -6,7 +6,7 @@ import android.net.UrlQuerySanitizer;
 import java.net.URL;
 
 public class Locations {
-    public String Location, Description, Address, City, Category, Key;
+    private String Location, Description, Address, City, Category, Key;
 
 
     public Locations(){}
@@ -20,6 +20,44 @@ public class Locations {
         this.Category = Category;
         this.Key = Key;
 
+    }
+    public String getLocation(){
+        return Location;
+    }
+    public String getDescription(){
+        return Description;
+    }
+    public String getAddress(){
+        return Address;
+    }
+    public String getCity(){
+        return City;
+    }
+    public String getCategory(){
+        return Category;
+    }
+    public String getKey(){
+        return Key;
+    }
+
+    //testing
+    public void setLocation(String name){
+        Location=name;
+    }
+    public void setDescription(String name){
+        Description=name;
+    }
+    public void setAddress(String name){
+        Address=name;
+    }
+    public void setCity(String name){
+        City=name;
+    }
+    public void setCategory(String name){
+        Category=name;
+    }
+    public void setKey(String name){
+        Key=name;
     }
 
 }
--- app/src/main/java/com/example/myapplication/ViewLocations.java
+++ app/src/main/java/com/example/myapplication/ViewLocations.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,10 +15,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class ViewLocations extends AppCompatActivity {
 
     TextView textView1;
     TextView textView2;
+    ArrayList<Locations> locationsArray = new ArrayList<Locations>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,24 +41,36 @@ public class ViewLocations extends AppCompatActivity {
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference ref = database.getReference("Locations");
-        ref.addValueEventListener(new ValueEventListener() {
+        //if for some reason Category is now undercase, just change "Category" to "category"
+        ref.orderByChild("Category").equalTo(category).addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Locations location = dataSnapshot.getValue(Locations.class);
-                System.out.println(location);
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    Locations location = postSnapshot.getValue(Locations.class);
+                    if(location.getCity().equals(city)) {
+                        locationsArray.add(location);
+                    }
+                }
+                for(int i=0;i<locationsArray.size();i++) {
+                    //show the names that are queried
+                    System.out.println("****"+ locationsArray.get(i).getLocation() + "****");
+                }
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public void onCancelled(@NonNull DatabaseError databaseError) {
                 System.out.println("The read failed: " + databaseError.getCode());
             }
         });
+
 
     }
 
     public boolean onOptionsItemSelected( MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
+                //emptying the array (just in case)
+                locationsArray.clear();
                 this.finish();
                 return true;
         }
